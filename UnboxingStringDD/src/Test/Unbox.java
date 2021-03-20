3b73e4048a48343a1b42337f272ab705d396a5bd
BUBBLEgummmm <YOUNG__HATER@WIN-1O0LBU3N24B>
2021-03-20 19:14:25 +0300

    First Commit
    testing commit

---
+++ UnboxingStringDD/src/Test/Unbox.java
@@ -0,0 +1,50 @@
+package Test;
+
+import java.util.Arrays;
+
+public class Unbox {
+	
+	public static void main(String[] args) {
+		
+		String str = "2[2[x]y]z";
+		System.out.print(unpackFunc(str.toCharArray()));
+		
+	}
+	
+	public static String unpackFunc(char[] str) {
+		
+		String finalStr = "";
+		int containerCount = 0, biggestContainer = 0, ratio = 0;
+		
+		for(int i = 0; i < str.length; i++) {
+			
+			if(Character.isLetter(str[i]) && containerCount == 0)
+				finalStr += str[i];
+			
+			if (Character.isDigit(str[i]) && containerCount == 0)
+				//почему выдает код элемента???
+				//ratio = Integer.valueOf(str[i]);
+                ratio = Integer.parseInt(String.valueOf(str[i]));				
+			
+			//поиск первого контейнера
+			if (str[i] == '[') {				
+				containerCount ++;
+				if(containerCount == 1)
+					biggestContainer = i;				
+			}
+							
+			//закрытие контейнера
+			if (str[i] == ']') {				
+				containerCount --;
+				if (containerCount == 0) {
+					finalStr += unpackFunc(Arrays.copyOfRange(str,biggestContainer+1,i)).repeat(ratio);
+					ratio = 0;
+					biggestContainer = 0;					
+				}
+			}
+		}
+		
+		return finalStr;
+		
+	}
+}
